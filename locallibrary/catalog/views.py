caadd8723f1dc3778929b72aebf6a8e9e053c983
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-08-07 11:19:48 +0200

    Book list and detail view, pagination

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Book, Author, BookInstance, Genre
 
@@ -29,3 +30,9 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
+class BookListView(generic.ListView):
+        model = Book
+        paginate_by = 5
+
+class BookDetailView(generic.DetailView):
+        model = Book
