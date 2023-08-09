af9e4997250dae0a514e73ccd2efe0e11ce2b915
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-08-09 11:22:01 +0200

    New functionality with loaning books

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -4,11 +4,13 @@ from .models import Author, Genre, Book, BookInstance
 
 admin.site.register(Genre)
 
+
 class BookInline(admin.TabularInline):
-    model= Book
+    model = Book
 
     def get_extra(self, request, obj=None, **kwargs):
         return 0
+
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
@@ -19,11 +21,13 @@ class AuthorAdmin(admin.ModelAdmin):
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
+
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
 
     def get_extra(self, request, obj=None, **kwargs):
         return 0
+
 
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
@@ -31,19 +35,19 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
 
+
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book', 'status', 'due_back', 'id')
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
+
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )
-
-
 
